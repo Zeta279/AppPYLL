eb173ee24f72fde11b96e64ec87365b47c4428b2
Ezequiel Acosta Baca <equi27@live.com.ar>
2025-12-09 00:22:27 -0300

    Inicio del proyecto

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+using System.Diagnostics;
+using AppPYLL.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AppPYLL.Controllers
+{
+    public class HomeController : Controller
+    {
+        public HomeController()
+        {
+
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
