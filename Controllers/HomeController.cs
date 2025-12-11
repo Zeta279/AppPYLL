feb1bf048c9e9346bbcff010042484d346b2cffd
Ezequiel Acosta Baca <equi27@live.com.ar>
2025-12-11 17:26:26 -0300

    Cambios estéticos página principal

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -13,6 +13,39 @@ namespace AppPYLL.Controllers
 
         public IActionResult Index()
         {
+            var ingresos = 0m;
+            var egresos = 0m;
+            var clientes = 20;
+
+            List<Pago> pagos;
+
+            pagos = new List<Pago>
+            {
+                new Pago { Monto = 2000m, FechaPago = DateTime.Now.AddDays(-10), Positivo = true, Descripcion = "Pago de cliente A" },
+                new Pago { Monto = 1500m, FechaPago = DateTime.Now.AddDays(-8), Positivo = true, Descripcion = "Pago de cliente B" },
+                new Pago { Monto = 3000m, FechaPago = DateTime.Now.AddDays(-5), Positivo = false, Descripcion = "Gasto A" },
+                new Pago { Monto = 2500m, FechaPago = DateTime.Now.AddDays(-3), Positivo = true, Descripcion = "Pago de cliente C" },
+                new Pago { Monto = 1000m, FechaPago = DateTime.Now.AddDays(-1), Positivo = false, Descripcion = "Gasto B" }
+            };
+
+            foreach(var pago in pagos)
+            {
+                if(pago.Positivo)
+                {
+                    ingresos += pago.Monto;
+                }
+                else
+                {
+                    egresos += pago.Monto;
+                }
+            }
+
+            ViewData["ingresos"] = ingresos.ToString("C2");
+            ViewData["egresos"] = egresos.ToString("C2");
+            ViewData["diferencia"] = (ingresos - egresos).ToString("C2");
+            ViewData["clientes"] = clientes;
+            ViewData["pagos"] = pagos;
+
             return View();
         }
 
---
+++ Models/Pago.cs
@@ -0,0 +1,15 @@
+﻿namespace AppPYLL.Models
+{
+    public class Pago
+    {
+        public int IDPago { get; set; }
+
+        public decimal Monto { get; set; }
+
+        public DateTime FechaPago { get; set; }
+
+        public bool Positivo { get; set; }
+
+        public string Descripcion { get; set; }
+    }
+}
